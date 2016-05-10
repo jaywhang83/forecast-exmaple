36c89f33dd6185d2d91d9fa6adc6c76822a9ebe2
Chris Kuiper <chris.kuiper@gmail.com>
2016-05-10 15:01:19 -0700

    add view for wind an d temp within a static location

--- src/Forecast/Models/Location.cs
+++ src/Forecast/Models/Location.cs
@@ -5,26 +5,24 @@ using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using ForecastIO;
-using Forecast.Models;
 
 namespace Forecast.Models
 {
     public class Location
     {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public double WindSpeed { get; set; }
-        public double Temperature { get; set; }
+        public float windSpeed { get; set; }
+        public float temperature { get; set; }
 
-        public static List<Location> GetLocations()
+        public static Dictionary<string,float> GetLocations()
         {
             var request = new ForecastIORequest(EnvironmentVariables.apiKey, 45.520705f, -122.677397f, Unit.si);
             var response = request.Get();
-
-            JObject jsonResponse = (JObject)JsonConvert.DeserializeObject(response.currently.ToString());
-
-            var locationlist = JsonConvert.DeserializeObject<List<Location>>(jsonResponse["locations"].ToString());
-            return locationlist;
+            var current = response.currently;
+            var loc = new Dictionary<string, float>();
+            loc.Add("temp", response.currently.temperature);
+            loc.Add("wind", response.currently.windSpeed);
+            return loc;
         }
+
     }
 }
