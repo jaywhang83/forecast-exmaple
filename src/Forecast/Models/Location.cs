0c6cbda7397e524d9f4ff416daa316b3cb1acf2a
Chris Kuiper <chris.kuiper@gmail.com>
2016-05-10 08:36:15 -0700

    add location model and action for locations' controller

---
+++ src/Forecast/Models/Location.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using ForecastIO;
+using Forecast.Models;
+
+namespace Forecast.Models
+{
+    public class Location
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double WindSpeed { get; set; }
+        public double Temperature { get; set; }
+
+        public static List<Location> GetLocations()
+        {
+            var request = new ForecastIORequest(EnvironmentVariables.apiKey, 45.520705f, -122.677397f, Unit.si);
+            var response = request.Get();
+
+            JObject jsonResponse = (JObject)JsonConvert.DeserializeObject(response.currently.ToString());
+
+            var locationlist = JsonConvert.DeserializeObject<List<Location>>(jsonResponse["locations"].ToString());
+            return locationlist;
+        }
+    }
+}
--- src/Forecast/Startup.cs
+++ src/Forecast/Startup.cs
@@ -6,29 +6,35 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace Forecast
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //1
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
+
+            //2
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
         }
-
-        // Entry point for the application.
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
